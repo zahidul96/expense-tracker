525ad4fb1443cd9cff8ff858347b04ae7a604521
zahidul96 <zahidulhussain111@gmail.com>
2026-07-23 20:14:55 +0600

    learned how to test code

---
+++ src/testComponents/product.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import ProductPage from "../pages/product/ProductPage";
+import userEvent from "@testing-library/user-event";
+import { expect, test, vi } from "vitest";
+import LoginForm from "../pages/login/loginComponents/LoginForm";
+test('testing prduct page', ()=>{
+    render(<ProductPage/>)
+    const productElement = screen.getByText('Product Page')
+    expect(productElement).toBeInTheDocument()
+})
+test('renders the product grid container', () => {
+  render(<ProductPage />);
+  const gridHeader = screen.getByText(/our products/i); 
+  expect(gridHeader).toBeInTheDocument();
+});
+test('renders the filter buttons for user selection', () => {
+  render(<ProductPage />);
+  const filterButton = screen.getByRole('button', { name: /filter/i });
+  expect(filterButton).toBeInTheDocument();
+});
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    // Enable global testing APIs like describe, test, and expect automatically
+    globals: true,
+    // Use jsdom to simulate a browser environment
+    environment: 'jsdom',
+    // Path to a setup file that runs before your tests start
+    setupFiles: './src/setupTests.js',
+  },
 })
